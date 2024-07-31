c3729dc3fc86ee49f17e95487ce51bee79cd858c
plusmail <plus4957@gmail.com>
2024-07-31 16:38:51 +0900

    두번째

--- src/main/java/kroryi/board/Main.java
+++ src/main/java/kroryi/board/Main.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 
 public class Main extends Application {
 //    private static final String STYLESHEET_PATH = "style.css";
-
+// 메인 고침
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LOGIN.fxml"));
